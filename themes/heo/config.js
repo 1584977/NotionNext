235e18fcb2fdd003492394775805a9f321349a0a
1584977 <86057621+1584977@users.noreply.github.com>
2024-09-05 00:26:40 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -7,10 +7,73 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+  HEO_NOTICE_BAR: import BLOG from '@/blog.config'
+import { siteConfig } from '@/lib/config'
+import { getGlobalData } from '@/lib/db/getSiteData'
+import { getLayoutByTheme } from '@/themes/theme'
+import { useRouter } from 'next/router'
+
+/**
+ * 搜索路由
+ * @param {*} props
+ * @returns
+ */
+const Search = props => {
+  const { posts } = props
+
+  // 根据页面路径加载不同Layout文件
+  const Layout = getLayoutByTheme({
+    theme: siteConfig('THEME'),
+    router: useRouter()
+  })
+
+  const router = useRouter()
+  const keyword = router?.query?.s
+
+  let filteredPosts
+  // 静态过滤
+  if (keyword) {
+    filteredPosts = posts.filter(post => {
+      const tagContent = post?.tags ? post?.tags.join(' ') : ''
+      const categoryContent = post.category ? post.category.join(' ') : ''
+      const searchContent =
+        post.title + post.summary + tagContent + categoryContent
+      return searchContent.toLowerCase().includes(keyword.toLowerCase())
+    })
+  } else {
+    filteredPosts = []
+  }
+
+  props = { ...props, posts: filteredPosts }
+
+  return <Layout {...props} />
+}
+
+/**
+ * 浏览器前端搜索
+ */
+export async function getStaticProps({ locale }) {
+  const props = await getGlobalData({
+    from: 'search-props',
+    locale
+  })
+  const { allPages } = props
+  props.posts = allPages?.filter(
+    page => page.type === 'Post' && page.status === 'Published'
+  )
+  return {
+    props,
+    revalidate: process.env.EXPORT
+      ? undefined
+      : siteConfig(
+          'NEXT_REVALIDATE_SECOND',
+          BLOG.NEXT_REVALIDATE_SECOND,
+          props.NOTION_CONFIG
+        )
+  }
+}
+
+export default Search
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
